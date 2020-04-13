54a3719b9cf12e6b0e1bbadc8247dfce3c700ea5
Dennise <63337204+dentolos19@users.noreply.github.com>
2020-04-13 16:27:34 +0800

    Revamped And Ported To .NET Core 3.1

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using WxInjector.Graphics;
+
+namespace WxInjector
+{
+
+    public partial class App
+    {
+
+        private void Initialize(object sender, StartupEventArgs e)
+        {
+            new WnMain().Show();
+        }
+
+    }
+
+}
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,14 +1,3 @@
-﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.InteropServices;
+using System.Windows;
 
-[assembly: AssemblyTitle("WxInjector")]
-[assembly: AssemblyCompany("Dennise")]
-[assembly: AssemblyProduct("WxInjector")]
-[assembly: AssemblyCopyright("Copyright © 2020")]
-[assembly: ComVisible(false)]
-[assembly: Guid("7967dddc-3786-4052-898d-bda43b6050c7")]
-[assembly: AssemblyVersion("1.1.2.0")]
-[assembly: AssemblyFileVersion("1.1.2.0")]
-[assembly: NeutralResourcesLanguage("en")]
-[assembly: AssemblyDescription("A Windows DLL Injector Made Entirely In .NET")]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
