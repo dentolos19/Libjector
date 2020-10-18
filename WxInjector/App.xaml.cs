88e7ce8840e0d15e0b6c1997e3dbd924754e46f1
Dennise Catolos <contact@dennise.me>
2020-10-18 16:59:54 +0800

    Removed App Center Analytics

--- WxInjector/App.xaml.cs
+++ WxInjector/App.xaml.cs
@@ -1,8 +1,5 @@
 ﻿using System.Windows;
 using System.Windows.Threading;
-using Microsoft.AppCenter;
-using Microsoft.AppCenter.Analytics;
-using Microsoft.AppCenter.Crashes;
 using WxInjector.Core;
 using WxInjector.Graphics;
 using AdonisMessageBox = AdonisUI.Controls.MessageBox;
@@ -17,7 +14,6 @@ namespace WxInjector
 
         private void InitializeApp(object sender, StartupEventArgs args)
         {
-            AppCenter.Start("12cb4cef-c869-46d8-86a7-b8d1efe2835e", typeof(Analytics), typeof(Crashes));
             Settings = Configuration.Load();
             if (!Utilities.IsRunningAsAdministrator())
             {
@@ -31,9 +27,6 @@ namespace WxInjector
         private void HandleError(object sender, DispatcherUnhandledExceptionEventArgs args)
         {
             args.Handled = true;
-            #if !DEBUG
-            Crashes.TrackError(args.Exception);
-            #endif
             new WnErrorHandler(args.Exception).ShowDialog();
         }
 
